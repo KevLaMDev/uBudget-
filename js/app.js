dee40ff5965ddd0441a990cfae02c75063b2e5c0
flowers-energy <zachary.t.flowers@gmail.com>
2021-12-24 02:44:42 +0700

    created chart in app.js, reformatted css on all pages removing disclaimer in footer, styling logo and images, and changing fonts

--- js/app.js
+++ js/app.js
@@ -2,19 +2,46 @@
 
 console.log('hello world');
 
-
 //Global variable section:
 
+// let capBudget = 0;
+let expenseArray = [1, 2, 3];
 
+// DOM
+
+
+// Constructor Function
+// function Expenses(div) {
+//   this.div = div.id;
+//   expenseArray.push(this);
+// }
+
+// object instances
+// new Expenses();
 // Functions:
 
 
 
 
+let ctx = document.getElementById('myChart').getContext('2d');
+let myChart = new Chart(ctx, { //eslint-disable-line
+  type: 'doughnut',
+  data: {
+    labels: ['rent', 'car', 'fun'],
+    datasets: [{
+      label: 'Expenses',
+      data: expenseArray,
+      rotation: 180,
+      borderColor: 'lightblue',
+      backgroundColor: [
+        'red',
+        'blue',
+        'coral'
+      ],
+      hoverOffset: 15
+    }]
+  },
+});
 
 
 
-
-
-
-//Popup Disclaimer
