c6b10d01dbbd32c7747c3e01894f32405ca8b1d3
joshuamccluskey <jpiff57@gmail.com>
2021-12-23 14:51:48 -0800

    removed disclaimer function will be replaced by modal popup disclaimer function

--- js/landing.js
+++ js/landing.js
@@ -7,10 +7,6 @@ let userArr = [];
 // buttonSubmit for form submission
 let buttonSubmit = document.getElementById('button-submit');
 
-//Legal Disclaimer function. This is the most basic solution. Stretch goal coul be to  build a more complex modal popup with HTML, CSS, and JS.
-// function disclaimer() {
-//   alert('Legal Disclaimer:\n\n' + 'The Content is for informational purposes only, you should not construe any such information or other material as legal, tax, investment, financial, or other advice. Nothing contained on our Site constitutes a solicitation, recommendation, endorsement, or offer by \u{03BC}Budget or any third party service provider to buy or sell any securities or other financial instruments in this or in in any other jurisdiction in which such solicitation or offer would be unlawful under the securities laws of such jurisdiction.\n\n' + 'All Content on this site is information of a general nature and does not address the circumstances of any particular individual or entity. Nothing in the Site constitutes professional and/or financial advice, nor does any information on the Site constitute a comprehensive or complete statement of the matters discussed or the law relating thereto. \u{03BC}Budget is not a fiduciary by virtue of any persons use of or access to the Site or Content. You alone assume the sole responsibility of evaluating the merits and risks associated with the use of any information or other Content on the Site before making any decisions based on such information or other Content. In exchange for using the Site, you agree not to hold \u{03BC}Budget, its affiliates or any third party service provider liable for any possible claim for damages arising from any decision you make based on information or other Content made available to you through the Site.');
-// }
 
 // Constructor function for user objects
 
@@ -370,9 +366,6 @@ function formBuilder() {
 }
 
 formBuilder();
-// disclaimer();
-
-
 
 //Event Handler function for form submission and instantiates object and saves values to local storage
 function handleFormSubmission(event) {
