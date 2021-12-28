4f707853de0fb5218b26dfc5be38e1f9978b55cc
joshuamccluskey <jpiff57@gmail.com>
2021-12-27 20:34:23 -0500

    Fisrt part of the form builder

--- js/landing.js
+++ js/landing.js
@@ -5,13 +5,21 @@
 //Global User Objects Array
 let userArr = [];
 let currentUser;
-// buttonSubmit for form submission
+let inputArr = ['housing', 'food', 'car', 'insurance', 'medical', 'phone', 'internet', 'utilities', 'entertainment', 'investing', 'retirement', 'savings', 'misc', 'other'];
+// Global form variables buttonSubmit for form submission
+let formTag = document.createElement('form');
+let fieldset = document.createElement('fieldset');
+let legend = document.createElement('legend');
+let div = document.createElement('div');
+let label = document.createElement('label');
+let input = document.createElement('input');
 let buttonSubmit = document.getElementById('button-submit');
+let addInputEl = document.getElementById('add-input-form');
 
 
-// // Constructor function for user objects
+// Constructor function for user objects
 
-function User(person = 'Joe Doe', income = 1, housing = 0, food = 1, car = 0, insurance = 0, medical = 0, phone = 0, internet = 0, utilities = 0, entertainment = 0, investing = 0, retirement = 0, savings = 0, misc = 0, other = 0) {
+function User(person = 'Joe Doe', income = 1, housing = 0, food = 0, car = 0, insurance = 0, medical = 0, phone = 0, internet = 0, utilities = 0, entertainment = 0, investing = 0, retirement = 0, savings = 0) {
   this.person = person;
   this.income = income;
   this.housing = housing;
@@ -26,14 +34,12 @@ function User(person = 'Joe Doe', income = 1, housing = 0, food = 1, car = 0, in
   this.investing = investing;
   this.retirement = retirement;
   this.savings = savings;
-  this.misc = misc;
-  this.other = other;
   this.afterTax = this.federalTaxCalc();
   this.totalExpenses = this.sumExpenses();
   userArr.push(this);
 }
 
-User.prototype.sumExpenses = function() {
+User.prototype.sumExpenses = function () {
   let sum = 0;
   sum += this.housing;
   sum += this.food;
@@ -51,7 +57,7 @@ User.prototype.sumExpenses = function() {
   return sum;
 };
 
-User.prototype.federalTaxCalc = function() {
+User.prototype.federalTaxCalc = function () {
   let tax;
   if (this.income >= 523, 601) tax = .37;
   if (this.income >= 209, 426) tax = .35;
@@ -64,31 +70,42 @@ User.prototype.federalTaxCalc = function() {
   return afterTax;
 };
 
+function buttonForm() {
+  //Form Submit Button
+  let fieldset = document.getElementById('expenses');
+  div = document.createElement('div');
+  div.id = 'button-submit';
+  fieldset.appendChild(div);
+  buttonSubmit = document.createElement('button');
+  buttonSubmit.type = 'submit';
+  buttonSubmit.textContent = 'Submit';
+  div.appendChild(buttonSubmit);
+}
 
 // Dynamic form builder function to allow the user to enter name, income, and budget information.
-function formBuilder() {
+function formMain() {
   //Creating initial elements needed for our DOM
   let section = document.getElementById('budget-form');
-  let formTag = document.createElement('form');
-  formTag.id = 'form-tag';
+  formTag = document.createElement('form');
+  formTag.id = 'form-main';
   section.appendChild(formTag);
-  let fieldset = document.createElement('fieldset');
+  fieldset = document.createElement('fieldset');
   fieldset.id = 'name-income';
   formTag.appendChild(fieldset);
-  let legend = document.createElement('legend');
+  legend = document.createElement('legend');
   legend.textContent = '\u{03BC}' + 'Budget Form';
   fieldset.appendChild(legend);
 
   //Fieldset Name and Income
   //Person Input Field of Form
-  let div = document.createElement('div');
+  div = document.createElement('div');
   div.id = 'person';
   fieldset.appendChild(div);
-  let label = document.createElement('label');
+  label = document.createElement('label');
   label.for = 'person';
   label.textContent = 'Name: ';
   div.appendChild(label);
-  let input = document.createElement('input');
+  input = document.createElement('input');
   input.type = 'text';
   input.name = 'person';
   input.id = 'person';
@@ -171,143 +188,8 @@ function formBuilder() {
   input.min = '0';
   label.appendChild(input);
 
-  //Insurance Input Field of Form
-  div = document.createElement('div');
-  div.id = 'insurance';
-  fieldset.appendChild(div);
-  label = document.createElement('label');
-  label.for = 'insurance';
-  label.textContent = 'Insurance: ';
-  div.appendChild(label);
-  input = document.createElement('input');
-  input.type = 'number';
-  input.name = 'insurance';
-  input.id = 'insurance';
-  input.placeholder = 'Ex. 1234';
-  input.step = 'any';
-  input.min = '0';
-  label.appendChild(input);
+  // //Savings Input Field of Form
 
-  //Medical/Healthcare Input Field of Form
-  div = document.createElement('div');
-  div.id = 'medical';
-  fieldset.appendChild(div);
-  label = document.createElement('label');
-  label.for = 'medical';
-  label.textContent = 'Medical/Healthcare: ';
-  div.appendChild(label);
-  input = document.createElement('input');
-  input.type = 'number';
-  input.name = 'medical';
-  input.id = 'medical';
-  input.placeholder = 'Ex. 1234';
-  input.step = 'any';
-  input.min = '0';
-  label.appendChild(input);
-
-  //Phone Input Field of Form
-  div = document.createElement('div');
-  div.id = 'phone';
-  fieldset.appendChild(div);
-  label = document.createElement('label');
-  label.for = 'phone';
-  label.textContent = 'Phone: ';
-  div.appendChild(label);
-  input = document.createElement('input');
-  input.type = 'number';
-  input.name = 'phone';
-  input.id = 'phone';
-  input.placeholder = 'Ex. 1234';
-  input.step = 'any';
-  input.min = '0';
-  label.appendChild(input);
-
-  //Internet Input Field of Form
-  div = document.createElement('div');
-  div.id = 'internet';
-  fieldset.appendChild(div);
-  label = document.createElement('label');
-  label.for = 'internet';
-  label.textContent = 'Internet: ';
-  div.appendChild(label);
-  input = document.createElement('input');
-  input.type = 'number';
-  input.name = 'internet';
-  input.id = 'internet';
-  input.placeholder = 'Ex. 1234';
-  input.step = 'any';
-  input.min = '0';
-  label.appendChild(input);
-
-  //Utilities Input Field of Form
-  div = document.createElement('div');
-  div.id = 'utilities';
-  fieldset.appendChild(div);
-  label = document.createElement('label');
-  label.for = 'utilities';
-  label.textContent = 'Utilities: ';
-  div.appendChild(label);
-  input = document.createElement('input');
-  input.type = 'number';
-  input.name = 'utilities';
-  input.id = 'utilities';
-  input.placeholder = 'Ex. 1234';
-  input.step = 'any';
-  input.min = '0';
-  label.appendChild(input);
-
-  //Entertainment Input Field of Form
-  div = document.createElement('div');
-  div.id = 'entertainment';
-  fieldset.appendChild(div);
-  label = document.createElement('label');
-  label.for = 'entertainment';
-  label.textContent = 'Entertainment: ';
-  div.appendChild(label);
-  input = document.createElement('input');
-  input.type = 'number';
-  input.name = 'entertainment';
-  input.id = 'entertainment';
-  input.placeholder = 'Ex. 1234';
-  input.step = 'any';
-  input.min = '0';
-  label.appendChild(input);
-
-  //Investing Input Field of Form
-  div = document.createElement('div');
-  div.id = 'investing';
-  fieldset.appendChild(div);
-  label = document.createElement('label');
-  label.for = 'investing';
-  label.textContent = 'Investing: ';
-  div.appendChild(label);
-  input = document.createElement('input');
-  input.type = 'number';
-  input.name = 'investing';
-  input.id = 'investing';
-  input.placeholder = 'Ex. 1234';
-  input.step = 'any';
-  input.min = '0';
-  label.appendChild(input);
-
-  //Retirement Input Field of Form
-  div = document.createElement('div');
-  div.id = 'retirement';
-  fieldset.appendChild(div);
-  label = document.createElement('label');
-  label.for = 'retirement';
-  label.textContent = 'Retirement: ';
-  div.appendChild(label);
-  input = document.createElement('input');
-  input.type = 'number';
-  input.name = 'retirement';
-  input.id = 'retirement';
-  input.placeholder = 'Ex. 1234';
-  input.step = 'any';
-  input.min = '0';
-  label.appendChild(input);
-
-  //Savings Input Field of Form
   div = document.createElement('div');
   div.id = 'savings';
   fieldset.appendChild(div);
@@ -324,43 +206,46 @@ function formBuilder() {
   input.min = '0';
   label.appendChild(input);
 
-  //Misc Input Field of Form
+
+
+  buttonForm();
+
+}
+
+formMain();
+
+function formBuilder() {
+  let section = document.getElementById('add-input-form');
+  formTag = document.createElement('form');
+  formTag.id = 'form-add';
+  section.appendChild(formTag);
+  fieldset = document.createElement('fieldset');
+  fieldset.id = 'more-input';
+  formTag.appendChild(fieldset);
+  legend = document.createElement('legend');
+  legend.textContent = 'Add Input';
+  fieldset.appendChild(legend);
   div = document.createElement('div');
-  div.id = 'misc';
+  div.id = 'add';
   fieldset.appendChild(div);
   label = document.createElement('label');
-  label.for = 'misc';
-  label.textContent = 'Misc : ';
+  label.for = 'add';
+  label.textContent = 'add: ';
   div.appendChild(label);
+
+  //Input
   input = document.createElement('input');
-  input.type = 'number';
-  input.name = 'misc';
-  input.id = 'misc';
-  input.placeholder = 'Ex. 1234';
+  input.type = 'text';
+  input.name = 'add';
+  input.id = 'add';
+  input.placeholder = 'Utilities, Internet';
   input.step = 'any';
   input.min = '0';
   label.appendChild(input);
 
-  // other Input Field of Form
+  //Button for Submit
   div = document.createElement('div');
-  div.id = 'other';
-  fieldset.appendChild(div);
-  label = document.createElement('label');
-  label.for = 'other';
-  label.textContent = 'Other: ';
-  div.appendChild(label);
-  input = document.createElement('input');
-  input.type = 'number';
-  input.name = 'other';
-  input.id = 'other';
-  input.placeholder = 'Ex. 1234';
-  input.step = 'any';
-  input.min = '0';
-  label.appendChild(input);
-
-  //Form Submit Button
-  div = document.createElement('div');
-  div.id = 'button-submit';
+  div.id = 'button-add-input';
   fieldset.appendChild(div);
   buttonSubmit = document.createElement('button');
   buttonSubmit.type = 'submit';
@@ -368,6 +253,31 @@ function formBuilder() {
   div.appendChild(buttonSubmit);
 
 }
+
+formBuilder();
+
+function handlerFormBuilder(event) {
+  let add = event.target.add.value;
+  let fieldset = document.getElementById('expenses');
+  div = document.createElement('div');
+  div.id = add;
+  fieldset.appendChild(div);
+  label = document.createElement('label');
+  label.for = add;
+  label.textContent = 'add/Healthcare: ';
+  div.appendChild(label);
+  input = document.createElement('input');
+  input.type = 'number';
+  input.name = add;
+  input.id = add;
+  input.placeholder = 'Ex. 1234';
+  input.step = 'any';
+  input.min = '0';
+  label.appendChild(input);
+
+}
+
+
 
 
 // //Event Handler function for form submission and instantiates object and saves values to local storage
@@ -379,22 +289,23 @@ function handleFormSubmission(event) {
   let housing = +event.target.housing.value;
   let food = +event.target.food.value;
   let car = +event.target.car.value;
-  let insurance = +event.target.insurance.value;
-  let medical = +event.target.medical.value;
-  let phone = +event.target.phone.value;
-  let internet = +event.target.internet.value;
-  let utilities = +event.target.utilities.value;
-  let entertainment = +event.target.entertainment.value;
-  let investing = +event.target.investing.value;
-  let retirement = +event.target.retirement.value;
   let savings = +event.target.savings.value;
-  let misc = +event.target.misc.value;
-  let other = +event.target.other.value;
+  // let insurance = +event.target.insurance.value;
+  // let medical = +event.target.medical.value;
+  // let phone = +event.target.phone.value;
+  // let internet = +event.target.internet.value;
+  // let utilities = +event.target.utilities.value;
+  // let entertainment = +event.target.entertainment.value;
+  // let investing = +event.target.investing.value;
+  // let retirement = +event.target.retirement.value;
+
 
 
   currentUser = person;
-  new User(person, income, housing, food, car, insurance, medical, phone, internet, utilities, entertainment, investing, retirement, savings, misc, other);
-  //Stingify the properties to be stored in local storage 
+  // new User(person, income, housing, food, car, insurance, medical, phone, internet, utilities, entertainment, investing, retirement, savings, misc, other);
+
+  new User(person, income, housing, food, car, savings);
+  //Stingify the properties to be stored in local storage
   let stringifiedUser = JSON.stringify(userArr);
 
   //Save to local storage
@@ -406,8 +317,6 @@ function handleFormSubmission(event) {
 }
 
 // Get the User
-
-
 
 let retrievedItem = localStorage.getItem('userObjects');
 
@@ -424,7 +333,174 @@ if (retrievedItem) {
   new User('Parent', 80000, 2000, 1000);
 }
 
-formBuilder();
+
+
 
 // Event Listeners
-document.querySelector('form').addEventListener('submit', handleFormSubmission);
+document.getElementById('form-main').addEventListener('submit', handleFormSubmission);
+document.getElementById('add-input').addEventListener('submit', handlerFormBuilder);
+
+
+// // //Insurance Input Field of Form
+// function insuranceInput() {
+//   let fieldset = document.getElementById('expenses');
+//   div = document.createElement('div');
+//   div.id = 'insurance';
+//   fieldset.appendChild(div);
+//   label = document.createElement('label');
+//   label.for = 'insurance';
+//   label.textContent = 'Insurance: ';
+//   div.appendChild(label);
+//   input = document.createElement('input');
+//   input.type = 'number';
+//   input.name = 'insurance';
+//   input.id = 'insurance';
+//   input.placeholder = 'Ex. 1234';
+//   input.step = 'any';
+//   input.min = '0';
+//   label.appendChild(input);
+
+// }
+
+
+// // //Medical/Healthcare Input Field of Form
+// function medicalInput() {
+//   let fieldset = document.getElementById('expenses');
+//   div = document.createElement('div');
+//   div.id = 'medical';
+//   fieldset.appendChild(div);
+//   label = document.createElement('label');
+//   label.for = 'medical';
+//   label.textContent = 'Medical/Healthcare: ';
+//   div.appendChild(label);
+//   input = document.createElement('input');
+//   input.type = 'number';
+//   input.name = 'medical';
+//   input.id = 'medical';
+//   input.placeholder = 'Ex. 1234';
+//   input.step = 'any';
+//   input.min = '0';
+//   label.appendChild(input);
+// }
+
+// // //Phone Input Field of Form
+// function phoneInput() {
+//   let fieldset = document.getElementById('expenses');
+//   div = document.createElement('div');
+//   div.id = 'phone';
+//   fieldset.appendChild(div);
+//   label = document.createElement('label');
+//   label.for = 'phone';
+//   label.textContent = 'Phone: ';
+//   div.appendChild(label);
+//   input = document.createElement('input');
+//   input.type = 'number';
+//   input.name = 'phone';
+//   input.id = 'phone';
+//   input.placeholder = 'Ex. 1234';
+//   input.step = 'any';
+//   input.min = '0';
+//   label.appendChild(input);
+// }
+
+// // //Internet Input Field of Form
+// function internetInput() {
+//   let fieldset = document.getElementById('expenses');
+//   div = document.createElement('div');
+//   div.id = 'internet';
+//   fieldset.appendChild(div);
+//   label = document.createElement('label');
+//   label.for = 'internet';
+//   label.textContent = 'Internet: ';
+//   div.appendChild(label);
+//   input = document.createElement('input');
+//   input.type = 'number';
+//   input.name = 'internet';
+//   input.id = 'internet';
+//   input.placeholder = 'Ex. 1234';
+//   input.step = 'any';
+//   input.min = '0';
+//   label.appendChild(input);
+// }
+
+// // //Utilities Input Field of Form
+// function utilitiesInput() {
+//   let fieldset = document.getElementById('expenses');
+//   div = document.createElement('div');
+//   div.id = 'utilities';
+//   fieldset.appendChild(div);
+//   label = document.createElement('label');
+//   label.for = 'utilities';
+//   label.textContent = 'Utilities: ';
+//   div.appendChild(label);
+//   input = document.createElement('input');
+//   input.type = 'number';
+//   input.name = 'utilities';
+//   input.id = 'utilities';
+//   input.placeholder = 'Ex. 1234';
+//   input.step = 'any';
+//   input.min = '0';
+//   label.appendChild(input);
+// }
+
+// // //Entertainment Input Field of Form
+// function entertainmentInput() {
+//   let fieldset = document.getElementById('expenses');
+//   div = document.createElement('div');
+//   div.id = 'entertainment';
+//   fieldset.appendChild(div);
+//   label = document.createElement('label');
+//   label.for = 'entertainment';
+//   label.textContent = 'Entertainment: ';
+//   div.appendChild(label);
+//   input = document.createElement('input');
+//   input.type = 'number';
+//   input.name = 'entertainment';
+//   input.id = 'entertainment';
+//   input.placeholder = 'Ex. 1234';
+//   input.step = 'any';
+//   input.min = '0';
+//   label.appendChild(input);
+// }
+
+// // //Investing Input Field of Form
+// function investingInput() {
+//   let fieldset = document.getElementById('expenses');
+//   div = document.createElement('div');
+//   div.id = 'investing';
+//   fieldset.appendChild(div);
+//   label = document.createElement('label');
+//   label.for = 'investing';
+//   label.textContent = 'Investing: ';
+//   div.appendChild(label);
+//   input = document.createElement('input');
+//   input.type = 'number';
+//   input.name = 'investing';
+//   input.id = 'investing';
+//   input.placeholder = 'Ex. 1234';
+//   input.step = 'any';
+//   input.min = '0';
+//   label.appendChild(input);
+// }
+
+// // //Retirement Input Field of Form
+// function retirementInput() {
+//   let fieldset = document.getElementById('expenses');
+//   div = document.createElement('div');
+//   div.id = 'retirement';
+//   fieldset.appendChild(div);
+//   label = document.createElement('label');
+//   label.for = 'retirement';
+//   label.textContent = 'Retirement: ';
+//   div.appendChild(label);
+//   input = document.createElement('input');
+//   input.type = 'number';
+//   input.name = 'retirement';
+//   input.id = 'retirement';
+//   input.placeholder = 'Ex. 1234';
+//   input.step = 'any';
+//   input.min = '0';
+//   label.appendChild(input);
+// }
+
+
